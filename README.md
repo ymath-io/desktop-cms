28402256b844aab894920133724dfc16927d4514
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2021-06-07 09:48:31 -0700

    it really does work

--- README.md
+++ README.md
@@ -6,12 +6,12 @@ npm install
 ```
 
 ### Compiles and hot-reloads for development
-```
+```bash
 npm run serve
 ```
 
 ### Compiles and minifies for production
-```
+```bash
 npm run build
 ```
 
--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ module.exports = {
     config.plugin('monaco-editor').use(MonacoWebpackPlugin, [
       {
         // Languages are loaded on demand at runtime
-        languages: ['scheme']
+        languages: ['markdown', 'yaml', 'javascript', 'html', 'css', 'json', 'xml']
       }
     ])
   }
